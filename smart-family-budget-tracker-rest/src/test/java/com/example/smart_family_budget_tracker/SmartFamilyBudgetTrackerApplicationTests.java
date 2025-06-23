6c0a90c9f6307f171b37300c24ee4db1cfacea45
Sagnik Samanta <sagnik2000samanta@gmail.com>
2025-06-24 01:47:10 +0530

    initial setup

---
+++ smart-family-budget-tracker-rest/src/test/java/com/example/smart_family_budget_tracker/SmartFamilyBudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_family_budget_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class SmartFamilyBudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ smart-family-budget-tracker-web/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      testing
+    </div>
+  );
+}
+
+export default App;
